a9daf3cbf2c2ab30f95f4f85f74157b954c1b182
Arslan <arslannasybullin@gmail.com>
2024-12-15 22:57:58 +0300

    navigation

---
+++ app/src/main/java/com/edalnik/edalnik/view/screens/FoodAppending.kt
@@ -0,0 +1,51 @@
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import me.bytebeats.views.charts.pie.PieChart
+import me.bytebeats.views.charts.pie.PieChartData
+import me.bytebeats.views.charts.pie.render.SimpleSliceDrawer
+import me.bytebeats.views.charts.simpleChartAnimation
+
+@Composable
+fun HomeScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFF121212)),
+        contentAlignment = Alignment.Center
+
+    ) {
+        Text(text = "Home Screen")
+        PieChartView()
+    }
+}
+
+@Composable
+fun PieChartView() {
+    PieChart(
+        pieChartData = PieChartData(
+            slices = listOf(
+                PieChartData.Slice(
+                    100F,
+                    Color.LightGray
+                ),
+                PieChartData.Slice(
+                    20F,
+                    Color.Yellow
+                ),
+            )
+        ),
+        // Optional properties.
+        modifier = Modifier
+            .size(200.dp),
+        animation = simpleChartAnimation(),
+        sliceDrawer = SimpleSliceDrawer(30F)
+    )
+}
+
+
