73ecc304d83b947dd9016c5c0ef951fe104f0f44
Arslan <arslannasybullin@gmail.com>
2024-12-19 21:49:09 +0300

    appending screen

--- app/src/main/java/com/edalnik/edalnik/view/screens/FoodAppending.kt
+++ app/src/main/java/com/edalnik/edalnik/view/screens/FoodAppending.kt
@@ -1,6 +1,7 @@
 package com.edalnik.edalnik.view.screens
 
 import android.annotation.SuppressLint
+import android.graphics.drawable.Icon
 import android.util.Log
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.expandVertically
@@ -18,6 +19,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -29,6 +31,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
@@ -111,7 +114,6 @@ fun ExpandableFoodList(
 
             ) {
                 items(chosenFoodItems) { foodItem ->
-                    Log.d("ABOBA","1")
                     FoodItemRow(
                         foodItem = foodItem,
                         onFoodRowClick = {
@@ -131,11 +133,25 @@ fun ExpandableFoodList(
                 isCartExpanded = !isCartExpanded
             },
             modifier = Modifier
-                .width(50.dp)
+                .width(190.dp)
                 .height(50.dp)
-                .padding(8.dp),
+                .padding(1.dp)
+
+            ,
+            border = BorderStroke(2.dp, Color(0xFF7E7E7E)),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFF393737),
+                contentColor = Color.White,
+                disabledContainerColor = Color.Gray,
+                disabledContentColor = Color.LightGray
+            ),
 
         ) {
+            Text(
+                text = if (isCartExpanded) "добавить продукты" else "закрыть список",
+                modifier = Modifier.padding(0.dp),
+                color = Color.White
+            )
         }
 
         AnimatedVisibility(
@@ -177,6 +193,16 @@ fun ExpandableFoodList(
                             }
                         )
                     }
+                }
+                if (filteredFoodItems.isEmpty()) {
+                    Text(
+                        text = "Такой еды в нашем списке нет",
+                        modifier = Modifier
+                            .padding(0.dp)
+                            .alpha(0.5F),
+                        color = Color.White
+
+                    )
                 }
             }
         }
